444fe87688c8d76cbb0c8ac3746d60f24b64ecb2
JaeHo Song <thruthesky@gmail.com>
2022-01-10 05:47:25 +0800

    phone sign in ui

--- example/lib/screens/phone_sign_in_ui/phone_sign_in_ui.screen.dart
+++ example/lib/screens/phone_sign_in_ui/phone_sign_in_ui.screen.dart
@@ -20,10 +20,47 @@ class _PhoneSignInUIScreenState extends State<PhoneSignInUIScreen> {
       body: Container(
         padding: const EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            /// You can add your custom design here.
+            ///
+            const Text('1. Select your country dial code'),
+
+            /// You can also do design with UI builders.
+            ///
+            /// [countryButtonBuilder] is the button design to select country dial code.
+            ///
+            /// [countrySelectedBuilder] is the design to display when country dial
+            /// code is selected.
+            /// It uses [country_code_picker](https://pub.dev/packages/country_code_picker)
+            /// package to choose country dial code. And CountryCode will be
+            /// passed on selectedBuilder
+            ///
+            /// [codeSent] handler will be invoked on sms verification code had
+            /// been sent. You may redirect the user to sms code input screen.
+            ///
+            /// [success] handler will be invoked only on Android devices that
+            /// support automatic SMS code resolution.
+            ///
+            /// [codeAutoRetrievalTimeout] handler will be invoked when code
+            /// auto retreival time is over.
+            ///
+            ///
+            /// [dialCodeStyle] is the dial code text style beside the input box.
+            ///
+            /// [phoneNumberInputDecoration] is the phone number input field decoration.
+            /// [phoneNumberInputTextStyle] is the text style of phone number.
+            ///
+            /// [submitButton] is the submit button. It's not a builder function.
+            ///
+            /// [inputTitle] is the widget to be shown on the input box when input box is visible.
+            ///
             PhoneNumberInput(
-              selectBuilder: () => const Text('Select Country Dial Code'),
-              selectedBuilder: (CountryCode code) {
+              countryButtonBuilder: () => Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(color: Colors.blue),
+                  child: const Text('Select Country Dial Code')),
+              countrySelectedBuilder: (CountryCode code) {
                 return Row(
                   children: [
                     Text(code.dialCode.toString()),
@@ -36,6 +73,24 @@ class _PhoneSignInUIScreenState extends State<PhoneSignInUIScreen> {
                   ],
                 );
               },
+              inputTitle: const Text('Please enter your phone number'),
+              dialCodeStyle: const TextStyle(fontSize: 32),
+              phoneNumberInputDecoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(8),
+              ),
+              phoneNumberInputTextStyle: const TextStyle(fontSize: 32),
+              submitTitle: const Text('Submit your phone number to verify'),
+              submitButton: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                child: const Text(
+                  'Verify phone number',
+                  style: TextStyle(color: Colors.white),
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
               codeSent: (id) => Get.toNamed('/sms-code-ui'),
               success: () => Get.defaultDialog(
                 middleText: 'Phone Sign In Success',
--- lib/src/auth/phone/phone_number_input.dart
+++ lib/src/auth/phone/phone_number_input.dart
@@ -7,23 +7,38 @@ class PhoneNumberInput extends StatefulWidget {
   PhoneNumberInput({
     this.favorites = const ['US', 'KR'],
     this.onChanged,
-    required this.selectBuilder,
-    required this.selectedBuilder,
+    required this.countryButtonBuilder,
+    required this.countrySelectedBuilder,
     required this.codeSent,
     required this.error,
     required this.success,
     required this.codeAutoRetrievalTimeout,
+    this.inputTitle = const SizedBox.shrink(),
+    this.dialCodeStyle = const TextStyle(fontSize: 24),
+    this.phoneNumberInputDecoration = const InputDecoration(),
+    this.phoneNumberInputTextStyle = const TextStyle(),
+    this.submitTitle = const SizedBox.shrink(),
+    this.submitButton = const Text(
+      'Submit',
+      style: TextStyle(color: Colors.blue, fontSize: 24),
+    ),
     Key? key,
   }) : super(key: key);
 
   final List<String> favorites;
   final void Function(CountryCode)? onChanged;
-  final Widget Function() selectBuilder;
-  final Widget Function(CountryCode) selectedBuilder;
+  final Widget Function() countryButtonBuilder;
+  final Widget Function(CountryCode) countrySelectedBuilder;
   final void Function(String verificationId) codeSent;
   final VoidCallback success;
   final ErrorCallback error;
   final void Function(String) codeAutoRetrievalTimeout;
+  final TextStyle dialCodeStyle;
+  final InputDecoration phoneNumberInputDecoration;
+  final TextStyle phoneNumberInputTextStyle;
+  final Widget submitButton;
+  final Widget inputTitle;
+  final Widget submitTitle;
 
   @override
   _PhoneNumberInputState createState() => _PhoneNumberInputState();
@@ -56,43 +71,60 @@ class _PhoneNumberInputState extends State<PhoneNumberInput> {
           },
           builder: (CountryCode? code) {
             return PhoneService.instance.selectedCode == null
-                ? widget.selectBuilder()
-                : widget.selectedBuilder(code!);
+                ? widget.countryButtonBuilder()
+                : widget.countrySelectedBuilder(code!);
           },
         ),
         if (PhoneService.instance.selectedCode != null)
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                PhoneService.instance.selectedCode!.dialCode!,
+              widget.inputTitle,
+              Row(
+                children: [
+                  Text(
+                    PhoneService.instance.selectedCode!.dialCode!,
+                    style: widget.dialCodeStyle,
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextField(
+                      style: widget.phoneNumberInputTextStyle,
+                      decoration: widget.phoneNumberInputDecoration,
+                      keyboardType: TextInputType.phone,
+                      onChanged: (t) {
+                        PhoneService.instance.domesticPhoneNumber = t;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: 8),
-              Expanded(
-                  child: TextField(
-                keyboardType: TextInputType.phone,
-                onChanged: (t) {
-                  PhoneService.instance.domesticPhoneNumber = t;
-                  setState(() {});
-                },
-              )),
             ],
           ),
         if (PhoneService.instance.domesticPhoneNumber != '' &&
             PhoneService.instance.codeSentProgress == false)
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                PhoneService.instance.codeSentProgress = true;
-              });
-              PhoneService.instance.phoneNumber = PhoneService.instance.completeNumber;
-              PhoneService.instance.verifyPhoneNumber(
-                codeSent: widget.codeSent,
-                success: widget.success,
-                error: widget.error,
-                codeAutoRetrievalTimeout: widget.codeAutoRetrievalTimeout,
-              );
-            },
-            child: Text('Submit'),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              widget.submitTitle,
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  setState(() {
+                    PhoneService.instance.codeSentProgress = true;
+                  });
+                  PhoneService.instance.phoneNumber = PhoneService.instance.completeNumber;
+                  PhoneService.instance.verifyPhoneNumber(
+                    codeSent: widget.codeSent,
+                    success: widget.success,
+                    error: widget.error,
+                    codeAutoRetrievalTimeout: widget.codeAutoRetrievalTimeout,
+                  );
+                },
+                child: widget.submitButton,
+              ),
+            ],
           ),
         if (PhoneService.instance.codeSentProgress) CircularProgressIndicator.adaptive(),
       ],
