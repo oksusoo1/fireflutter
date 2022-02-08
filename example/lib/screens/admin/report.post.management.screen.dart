12c6bfadfa19dea50556eb1027ec952565085041
Selrahc <pinedaclp@gmail.com>
2022-02-08 13:09:03 +0800

    update on report

--- example/lib/screens/admin/report.post.management.screen.dart
+++ example/lib/screens/admin/report.post.management.screen.dart
@@ -17,23 +17,32 @@ class ReportPostManagementScreen extends StatelessWidget {
       body: ReportPostManagement(
         id: Get.arguments['id'],
         onError: error,
-        builder: (PostModel post) {
+        builder: (post) {
           print(target);
           return Column(
             children: [
               Text(post.content),
               Text(post.id),
-              ElevatedButton(
-                onPressed: () {
-                  print('mark as deleted');
-                },
-                child: const Text('Delete'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('mark as resolve');
-                },
-                child: const Text('Resolve'),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        await post.delete();
+                        alert('Post deleted', 'You have deleted this post.');
+                      } catch (e) {
+                        error(e);
+                      }
+                    },
+                    child: const Text('Delete'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('mark as resolve');
+                    },
+                    child: const Text('Resolve'),
+                  ),
+                ],
               ),
             ],
           );
