7f001b5efadd2427dd560d132d31dfe815e12e40
JAEHO SONG <thruthesky@JAEHOs-MacBook-Pro.local>
2022-02-18 09:55:45 +0900

    testing getCommentAncestors

--- firebase/functions/lib.js
+++ firebase/functions/lib.js
@@ -1,13 +1,131 @@
-  // get comment ancestor by getting parent comment until it reach the root comment
-  // return the uids of the author
+"use strict";
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+
+// get firestore
+const db = admin.firestore();
+
+function commentDoc(id) {
+  return db.collection('comments').doc(id);
+}
+
+
+function getCategories()  {
+  return db.collection("categories").get();
+}
+
+async function getSizeOfCategories() {
+  const snapshot = await getCategories();
+  return snapshot.size;
+}
+
+async function createCategory(data) {
+  return db.collection('categories').doc(
+    data.category ? data.category : 'test',
+  ).set({title: 'create category'});
+}
+
+/**
+ * 
+ * @returns reference
+ */
+async function createPost(data) {
+  const postData = {
+    category: data.category ? data.category : 'test',
+    title: data.title ? data.title : 'create_post',
+    uid: data.uid ? data.uid : 'uid',
+  };
+  if ( data.post.id ) {
+    await db.collection('posts').doc(data.post.id).set(postData);
+    return db.collection('posts').doc(data.post.id);
+  }
+  return db.collection('posts').add(postData);
+}
+
+/**
+ * 
+ * @returns reference
+ * 
+ * 
+ * await lib.createComment({
+    category: 'test',         // create a category
+    post: {                   // post
+        id: 'post_id_a',      // if post id exists, it sets. or create.
+        title: 'post_title',
+        uid: 'A',
+    },
+    comment: {
+        id: 'comment_id_a',         // if comment id exists, it sets. or create.
+        content: 'comment_content',
+        uid: 'B',
+    }
+  });
+
+  *
+  * since
+  *   - there is no category, category is not created.
+  *   - there is no post, post is not created.
+  * 
+  await lib.createComment({
+    comment: {
+        id: 'comment_id_a',         // if comment id exists, it sets. or create.
+        postId: 'post_id_a',
+        parentId: 'comemnt_id_a',
+        content: 'comment_content',
+        uid: 'B',
+      }
+  });
+ */
+async function createComment(data) {
+  if ( data.category) await createCategory(data);
+  
+  let commentData;
+  // If there is no postId in data, then create one.
+  if ( data.post && !data.post.postId ) {
+    const ref = await createPost(data);
+    commentData = {
+      postId: ref.id,
+      parentId: ref.id,
+      content: 'create comment',
+      uid: data.comment.uid ? data.comment.uid : 'uid',
+    };
+  } else {
+    commentData = {
+      postId: data.comment.postId,
+      parentId: data.comment.parentId,
+      content: data.comment.content ? data.comment.content : '',
+      uid: data.comment.uid ? data.comment.uid : 'uid',
+    };
+  }
+  // if no comment id, then create one
+  if ( ! data.comment.id ) return db.collection('comments').add(commentData);
+  else {
+    await commentDoc(data.comment.id).set(commentData);
+    return commentDoc(data.comment.id);
+  }
+}
+
+exports.getSizeOfCategories = getSizeOfCategories;
+exports.getCategories = getCategories;
+exports.createCategory = createCategory;
+exports.createPost = createPost;
+exports.createComment = createComment;
+
+
+
+
+// get comment ancestor by getting parent comment until it reach the root comment
+// return the uids of the author
  exports.getCommentAncestors = async function (id, authorUid) {
-    let comment = await admin.firestore().collection('comments').doc(id).get();
+    let comment = await commentDoc(id).get();
     const uids = [];
     while(true) {
       if (comment.data().postId == comment.data().parentId ) break;
-      comment = await admin.firestore().collection('comments').doc(comment.data().parentId).get();
+      comment = await commentDoc(comment.data().parentId).get();
       if(comment.exists == false) continue;
-      if(comment.data().uid == authorUid) continue; //get author uid.
+      if(comment.data().uid == authorUid) continue; // skip the author's uid.
       uids.push(comment.data().uid);
     }
     return uids.filter((v, i, a) => a.indexOf(v) === i);  // remove duplicate
--- firebase/functions/tests/test.js
+++ firebase/functions/tests/test.js
@@ -6,10 +6,6 @@ const assert = require("assert");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-const lib = require("../lib");
-
-
-
 // initialize the firebase
 if (!admin.apps.length) {
     const serviceAccount = require("../../withcenter-test-project.adminKey.json");
@@ -17,33 +13,93 @@ if (!admin.apps.length) {
         credential: admin.credential.cert(serviceAccount),
     });
 }
-
+// This must come after initlization
+const lib = require("../lib");
 
 // get firestore
 const db = admin.firestore();   
 
-async function createCategory(category) {
-    return db.collection('categories').doc(category).set({title: 'create category'});
-}
-async function createPost(category) {
-    return db.collection('posts').add({ category: category, title: 'create post'});
-}
 
-function createComment() {
-    await createCategory('test');
-    const ref = await createPost('test');
-    return db.collection('comments').add({postId: ref.id, parentId: ref.id, content: 'create comment'});
-}
 
 describe("Categories", () => {
     it("Get categories", async () => {
-        const snapshot = await db.collection("categories").get();
-        const size = snapshot.size;
-        console.log('size; ', size);
+        const size = await lib.getSizeOfCategories();
         assert.ok( size > 0);
     });
-    it("Prepare data", async() => {
-        await createComment();
+
+
+    it("get comment anscestor uid", async() => {
+        const parent = await lib.createComment({
+            category: 'test',
+            post: {
+                id: 'Pid-1',
+                title: 'post_title',
+                uid: 'A',
+            },
+            comment: {
+                id: 'Cid-1',
+                postId: 'Pid-1',
+                parentId: 'Pid-1',
+                content: 'comment_content',
+                uid: 'B',
+            }
+        });
+        
+
+        await lib.createComment({
+            comment: {
+                id: 'Cid-2',
+                postId: 'Pid-1',
+                parentId: 'Cid-1',
+                content: 'comment_content',
+                uid: 'B',
+            }
+        });
+
+        let res = await lib.getCommentAncestors('Cid-2', 'C');
+
+        assert.ok( res.length == 1 && res[0] == 'B' );
+
+        // expect ok. res.length == 1
+        // Add a comment with same author uid.
+        await lib.createComment({
+            comment: {
+                id: 'Cid-3',
+                postId: 'Pid-1',
+                parentId: 'Cid-2',
+                uid: 'C',
+            }
+        });
+        res = await lib.getCommentAncestors('Cid-3', 'C');
+        assert.ok( res.length == 1 && res[0] == 'B' );
+
+
+        // expect ok. res.length == 1.
+        // Add a comment with different author, but still result is 1 since the current
+        // comment is excluded.
+        await lib.createComment({
+            comment: {
+                id: 'Cid-4',
+                postId: 'Pid-1',
+                parentId: 'Cid-3',
+                uid: 'D',
+            }
+        });
+        res = await lib.getCommentAncestors('Cid-4', 'C');
+        assert.ok( res.length == 1 && res[0] == 'B' );
+
+        // expect ok. res.length == 2.
+        // Add a comment with different author
+        await lib.createComment({
+            comment: {
+                id: 'Cid-5',
+                postId: 'Pid-1',
+                parentId: 'Cid-4',
+                uid: 'E',
+            }
+        });
+        res = await lib.getCommentAncestors('Cid-5', 'C');
+        assert.ok( res.length == 2 && res[0] == 'D' && res[1] == 'B' );
+
     });
-   });
-   
+});
