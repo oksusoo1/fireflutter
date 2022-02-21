0477294fb42cae6a33ef86ba93285b62c59bbde8
JaeHo Song <thruthesky@gmail.com>
2022-02-21 18:22:07 +0900

    updat eon context

--- firebase/functions/tests/admin.messaging.test.js
+++ firebase/functions/tests/admin.messaging.test.js
@@ -8,27 +8,22 @@ const admin = require("firebase-admin");
 
 // initialize the firebase
 if (!admin.apps.length) {
-    const serviceAccount = require("../../withcenter-test-project.adminKey.json");
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: 'https://withcenter-test-project-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    });
+  const serviceAccount = require("../../withcenter-test-project.adminKey.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://withcenter-test-project-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  });
 }
 // This must come after initlization
 const lib = require("../lib");
 
 // get firestore
-const db = admin.firestore();   
+const db = admin.firestore();
 
 describe("Admin Messaging ~~~~~~~~~~~~~~~~", () => {
-
-
-    it("Admin sending push notification.", async() => {
-        assert.ok( res.length == 1 && res[0] == 'B' );
-
-    });
-    
-
+  it("Admin sending push notification.", async () => {
+    assert.ok( res.length == 1 && res[0] == "B" );
   });
+});
 
 
