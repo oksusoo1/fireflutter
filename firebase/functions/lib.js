5334c921b4166ad7ee64e57871ba5002389a2e0d
JaeHo Song <thruthesky@gmail.com>
2022-02-22 17:46:50 +0900

    update on time

--- firebase/functions/lib.js
+++ firebase/functions/lib.js
@@ -24,9 +24,9 @@ const delay = (time) => new Promise((res)=>setTimeout(res, time));
  * @return int unix timestamp
  */
 function timestamp() {
-  // return Math.round( (new Date).getTime() / 1000 );
-  return Math.round( (new Date).getTime() );
+  return Math.round( (new Date).getTime() / 1000 );
 }
+
 
 /**
  * Returns category referrence
