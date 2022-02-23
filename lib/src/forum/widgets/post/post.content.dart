b2de330a98a65439edc38bc8f04ae4a5e2366099
JaeHo Song <thruthesky@gmail.com>
2022-02-23 16:37:08 +0900

    update on forum view

--- lib/src/forum/widgets/post/post.content.dart
+++ lib/src/forum/widgets/post/post.content.dart
@@ -1,37 +1,36 @@
+import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
-import 'package:wonderfulkorea/widgets/common/html_content.dart';
-import 'package:wonderfulkorea/packages/local/wordpress/lib/wordpress.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ForumListPostContent extends StatelessWidget {
-  const ForumListPostContent(this.post, {this.withImage = true, Key? key}) : super(key: key);
+class PostContent extends StatelessWidget {
+  const PostContent(this.post, {this.withImage = true, Key? key})
+      : super(key: key);
 
-  final WPPost post;
+  final PostModel post;
   final bool withImage;
 
   @override
   Widget build(BuildContext context) {
-    // printLongString(post.content);
     return Column(
       children: [
         Divider(color: Colors.grey),
         Container(
-          padding: EdgeInsets.symmetric(horizontal: sm, vertical: xs),
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
           decoration: BoxDecoration(color: Colors.white),
           child: () {
             /// Youtube link 가 있다면, (HTML 이 있든 없든)
-            if (hasYoutubeLink(post.content)) {
-              String content = convertYoutubeLinkToEmbedHTML(post.content);
-              return HtmlContent(content);
-            } else if (post.html)
-
-              /// Youtube link 는 없지만, HTML 이면,
-              return HtmlContent(post.content);
+            // if (hasYoutubeLink(post.content)) {
+            //   String content = convertYoutubeLinkToEmbedHTML(post.content);
+            //   return HtmlContent(content);
+            // } else if (post.html)
+            if (post.isHtmlContent)
+              return Html(data: post.displayContent);
             else
               return SelectableLinkify(
-                text: post.content,
+                text: post.displayContent,
                 onOpen: (link) async {
                   if (await canLaunch(link.url)) {
                     await launch(link.url);
