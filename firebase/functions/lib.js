0dece2605ca9bf84fd4bab71676cd1fd2610d43e
Selrahc <pinedaclp@gmail.com>
2022-03-03 18:13:49 +0800

    lint fix

--- firebase/functions/lib.js
+++ firebase/functions/lib.js
@@ -114,9 +114,9 @@ function indexForumDocument(data) {
 async function deleteIndexedPostDocument(id) {
   const promises = [];
   promises.push(
-    Axios.post("https://wonderfulkorea.kr:4431/index.php?api=post/delete", {
-      id: id,
-    })
+      Axios.post("https://wonderfulkorea.kr:4431/index.php?api=post/delete", {
+        id: id,
+      }),
   );
   promises.push(Axios.delete("http://wonderfulkorea.kr:7700/indexes/posts/documents/" + id));
   promises.push(deleteIndexedForumDocument(id));
@@ -126,9 +126,9 @@ async function deleteIndexedPostDocument(id) {
 async function deleteIndexedCommentDocument(id) {
   const promises = [];
   promises.push(
-    Axios.post("https://wonderfulkorea.kr:4431/index.php?api=post/delete", {
-      id: id,
-    })
+      Axios.post("https://wonderfulkorea.kr:4431/index.php?api=post/delete", {
+        id: id,
+      }),
   );
   promises.push(Axios.delete("http://wonderfulkorea.kr:7700/indexes/comments/documents/" + id));
   promises.push(deleteIndexedForumDocument(id));
@@ -225,9 +225,9 @@ async function sendMessageToTopic(query) {
   const payload = topicPayload(query);
   try {
     const res = await admin.messaging().send(payload);
-    return { code: "success", result: res };
+    return {code: "success", result: res};
   } catch (e) {
-    return { code: "error", message: e };
+    return {code: "error", message: e};
   }
 }
 
@@ -243,9 +243,9 @@ async function sendMessageToTokens(query) {
 
   try {
     const res = await sendingMessageToTokens(_tokens, payload);
-    return { code: "success", result: res };
+    return {code: "success", result: res};
   } catch (e) {
-    return { code: "error", message: e };
+    return {code: "error", message: e};
   }
 }
 
@@ -255,9 +255,9 @@ async function sendMessageToUsers(query) {
   // console.log(tokens);
   try {
     const res = await sendingMessageToTokens(tokens, payload);
-    return { code: "success", result: res };
+    return {code: "success", result: res};
   } catch (e) {
-    return { code: "error", message: e };
+    return {code: "error", message: e};
   }
 }
 
@@ -312,7 +312,7 @@ async function sendingMessageToTokens(tokens, payload) {
     });
   });
   await Promise.all(tokensToRemove);
-  return { success: successCount, error: errorCount };
+  return {success: successCount, error: errorCount};
 }
 
 function topicPayload(topic, query) {
@@ -392,24 +392,26 @@ async function isAdmin(context) {
 }
 
 async function enableUser(data, context) {
-  if(!isAdmin(context))
-  return {
-    code: "ERROR_YOU_ARE_NOT_ADMIN",
-    message: "To manage user, you need to sign-in as an admin.",
-  };
-  await rdb.ref('users').child(data.uid).update({ disabled: false })
-  return auth.updateUser(data.uid, { disabled: false });
+  if (!isAdmin(context)) {
+    return {
+      code: "ERROR_YOU_ARE_NOT_ADMIN",
+      message: "To manage user, you need to sign-in as an admin.",
+    };
+  }
+  await rdb.ref("users").child(data.uid).update({disabled: false});
+  return auth.updateUser(data.uid, {disabled: false});
 }
 
 async function disableUser(data, context) {
   const admin = await isAdmin(context);
-  if(!admin)
-  return {
-    code: "ERROR_YOU_ARE_NOT_ADMIN",
-    message: "To manage user, you need to sign-in as an admin.",
-  };
-  await rdb.ref('users').child(data.uid).update({ disabled: true })
-  return auth.updateUser(data.uid, { disabled: true });
+  if (!admin) {
+    return {
+      code: "ERROR_YOU_ARE_NOT_ADMIN",
+      message: "To manage user, you need to sign-in as an admin.",
+    };
+  }
+  await rdb.ref("users").child(data.uid).update({disabled: true});
+  return auth.updateUser(data.uid, {disabled: true});
 }
 
 exports.delay = delay;
--- firebase/functions/tests/user.management.test.js
+++ firebase/functions/tests/user.management.test.js
@@ -40,89 +40,85 @@ describe("Admin user management ~~~~~~~~~~~~~~~~", () => {
     await test.createTestUser(userA);
     await test.createTestUser(userB);
     await test.createTestUser(userC);
-    
+
     // set userA as admin
-    await db.collection('settings').doc('admins').set({[userA]: true}, {merge: true});
+    await db.collection("settings").doc("admins").set({[userA]: true}, {merge: true});
     try {
-        const re = await  lib.disableUser({uid: userB}, {});
-        if ( re.code ==  'ERROR_YOU_ARE_NOT_ADMIN') assert.ok("should be error since user not provided");
-        else assert.fail("must be error with ERROR_YOU_ARE_NOT_ADMIN, but got: " + re);
-    } catch(e) {
-        assert.fail("should be error with ERROR_YOU_ARE_NOT_ADMIN" + e);
+      const re = await lib.disableUser({uid: userB}, {});
+      if ( re.code == "ERROR_YOU_ARE_NOT_ADMIN") assert.ok("should be error since user not provided");
+      else assert.fail("must be error with ERROR_YOU_ARE_NOT_ADMIN, but got: " + re);
+    } catch (e) {
+      assert.fail("should be error with ERROR_YOU_ARE_NOT_ADMIN" + e);
     }
 
     try {
-        const re = await  lib.disableUser({uid: userB}, {auth: {uid: userC }});
-        if ( re.code ==  'ERROR_YOU_ARE_NOT_ADMIN') assert.ok("should be error since user is not admin");
-        else assert.fail("must be error with ERROR_YOU_ARE_NOT_ADMIN, but got: " + re);
-    } catch(e) {
-        assert.fail("should be error with ERROR_YOU_ARE_NOT_ADMIN" + e);
+      const re = await lib.disableUser({uid: userB}, {auth: {uid: userC}});
+      if ( re.code == "ERROR_YOU_ARE_NOT_ADMIN") assert.ok("should be error since user is not admin");
+      else assert.fail("must be error with ERROR_YOU_ARE_NOT_ADMIN, but got: " + re);
+    } catch (e) {
+      assert.fail("should be error with ERROR_YOU_ARE_NOT_ADMIN" + e);
     }
 
     try {
-        const re = await  lib.disableUser({uid: userB}, {auth: {uid: userA}});
-        const c = await  lib.disableUser({uid: userC}, {auth: {uid: userA}});
-        assert.ok(re.uid == userB, "userB uid");
-        assert.ok(re.disabled == true, "userB must be disabled true");
-        assert.ok(c.disabled == true, "userC must be disabled true");
-    } catch(e) {
-        assert.fail("must be no error: " + e);
-    }
-
-    
-    
-    try {
-        const res = await  rdb.ref('users').orderByChild('disabled').equalTo(true).get();
-        assert.ok(res.exists(), 'users must exist with disable true');
-        const users = res.val();
-        assert.ok(users[userB].disabled == true, 'userB must exist with disable true');
-        assert.ok(users[userC].disabled == true, 'userC must exist with disable true');
-    } catch(e) {
-        assert.fail("query must be no error: " + e);
+      const re = await lib.disableUser({uid: userB}, {auth: {uid: userA}});
+      const c = await lib.disableUser({uid: userC}, {auth: {uid: userA}});
+      assert.ok(re.uid == userB, "userB uid");
+      assert.ok(re.disabled == true, "userB must be disabled true");
+      assert.ok(c.disabled == true, "userC must be disabled true");
+    } catch (e) {
+      assert.fail("must be no error: " + e);
     }
 
 
     try {
-        const res = await rdb.ref('users').child(userB).get();
-        const user = res.val();
-        assert.ok(res.exists, 'user must exist');
-        assert.ok(user.disabled == true, 'user marked as disabled')
-    } catch(e) {
-        assert.fail('user must exist and marked as disabled but got error: ' + e);
+      const res = await rdb.ref("users").orderByChild("disabled").equalTo(true).get();
+      assert.ok(res.exists(), "users must exist with disable true");
+      const users = res.val();
+      assert.ok(users[userB].disabled == true, "userB must exist with disable true");
+      assert.ok(users[userC].disabled == true, "userC must exist with disable true");
+    } catch (e) {
+      assert.fail("query must be no error: " + e);
     }
 
 
     try {
-        const b = await  lib.enableUser({uid: userB}, {auth: {uid: userA}});       
-        const c = await  lib.enableUser({uid: userC}, {auth: {uid: userA}});
-        assert.ok(b.uid == userB, "userB uid for enabling user");
-        assert.ok(b.disabled == false, "userB must be disabled false");
-        assert.ok(c.disabled == false, "userC must be disabled false");
-    } catch(e) {
-        assert.fail("must be no error: " + e);
-    }
-
-    try {
-        const res = await  rdb.ref('users').orderByChild('disabled').equalTo(false).get();
-        assert.ok(res.exists(), 'users must exist with disable true');
-        const users = res.val();
-        assert.ok(users[userB].disabled == false, 'userB must exist with disable false');
-        assert.ok(users[userC].disabled == false, 'userC must exist with disable false');
-    } catch(e) {
-        assert.fail("query must be no error: " + e);
+      const res = await rdb.ref("users").child(userB).get();
+      const user = res.val();
+      assert.ok(res.exists, "user must exist");
+      assert.ok(user.disabled == true, "user marked as disabled");
+    } catch (e) {
+      assert.fail("user must exist and marked as disabled but got error: " + e);
     }
 
 
     try {
-        const res = await rdb.ref('users').child(userB).get();
-        const user = res.val();
-        assert.ok(user.disabled == false, 'user marked as disabled as false')
-    } catch(e) {
-        assert.fail('user disabled must be false but got error: ' + e);
+      const b = await lib.enableUser({uid: userB}, {auth: {uid: userA}});
+      const c = await lib.enableUser({uid: userC}, {auth: {uid: userA}});
+      assert.ok(b.uid == userB, "userB uid for enabling user");
+      assert.ok(b.disabled == false, "userB must be disabled false");
+      assert.ok(c.disabled == false, "userC must be disabled false");
+    } catch (e) {
+      assert.fail("must be no error: " + e);
+    }
+
+    try {
+      const res = await rdb.ref("users").orderByChild("disabled").equalTo(false).get();
+      assert.ok(res.exists(), "users must exist with disable true");
+      const users = res.val();
+      assert.ok(users[userB].disabled == false, "userB must exist with disable false");
+      assert.ok(users[userC].disabled == false, "userC must exist with disable false");
+    } catch (e) {
+      assert.fail("query must be no error: " + e);
     }
 
 
-      
+    try {
+      const res = await rdb.ref("users").child(userB).get();
+      const user = res.val();
+      assert.ok(user.disabled == false, "user marked as disabled as false");
+    } catch (e) {
+      assert.fail("user disabled must be false but got error: " + e);
+    }
   });
 });
 
