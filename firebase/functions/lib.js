e72275a60f027e184169d04b1f553ac63683382c
YinG17 <simbulanja.17@gmail.com>
2022-03-15 12:55:02 +0800

    user index deletion update

--- firebase/functions/lib.js
+++ firebase/functions/lib.js
@@ -85,8 +85,8 @@ async function indexUserDocument(uid, data = {}) {
 async function deleteIndexedUserDocument(uid) {
   const promises = [];
   rdb.ref("users").child(uid).exists
-  // promises.push(rdb.ref("users").child(uid).remove());
-  // promises.push(rdb.ref("user-settings").child(uid).remove());
+  promises.push(rdb.ref("users").child(uid).remove());
+  promises.push(rdb.ref("user-settings").child(uid).remove());
   promises.push(Axios.delete("http://wonderfulkorea.kr:7700/indexes/users/documents/" + uid));
   return Promise.all(promises);
 }
