90e7d8025069709b591f87cf389885fd17091ca7
JaeHo Song <thruthesky@gmail.com>
2022-03-19 18:24:50 +0900

    chat ui. secuirty rules

--- lib/src/chat/widgets/chat.badge.dart
+++ lib/src/chat/widgets/chat.badge.dart
@@ -30,7 +30,7 @@ class _ChatBadgeState extends State<ChatBadge> {
       shape: BadgeShape.circle,
       badgeColor: Colors.red,
       elevation: 0,
-      padding: EdgeInsets.all(2.0),
+      padding: EdgeInsets.all(3.0),
       badgeContent: Text(
         no.toString(),
         style: TextStyle(color: Colors.white, fontSize: 11),
