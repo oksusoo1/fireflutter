f39567bc590b2c1f2843b6892b5b02611503e293
JaeHo Song <thruthesky@gmail.com>
2022-03-22 23:01:31 +0900

    update for friend_map & map

--- lib/src/friend_map/friend_map.dart
+++ lib/src/friend_map/friend_map.dart
@@ -67,8 +67,7 @@ class _FriendMapState extends State<FriendMap> with WidgetsBindingObserver {
 
   initPositionListener() {
     positionStream = service.initLocationListener().listen((Position position) {
-      print(
-          'position changed: lat ${position.latitude} ; lng ${position.longitude}');
+      print('position changed: lat ${position.latitude} ; lng ${position.longitude}');
 
       service.updateMarkerPosition(
         MarkerIds.currentLocation,
@@ -83,7 +82,7 @@ class _FriendMapState extends State<FriendMap> with WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
-    print('state $state');
+    // print('state $state');
 
     if (state == AppLifecycleState.resumed && !service.locationServiceEnabled) {
       markUsersLocations();
@@ -101,8 +100,7 @@ class _FriendMapState extends State<FriendMap> with WidgetsBindingObserver {
           zoomControlsEnabled: false,
           myLocationButtonEnabled: false,
           initialCameraPosition: currentLocation,
-          onMapCreated: (GoogleMapController controller) =>
-              service.mapController = controller,
+          onMapCreated: (GoogleMapController controller) => service.mapController = controller,
           markers: Set<Marker>.from(service.markers),
           polylines: Set<Polyline>.of(service.polylines.values),
         ),
