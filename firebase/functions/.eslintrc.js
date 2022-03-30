147064900546c182fbdd16b3fd150c7c266f240c
JaeHo Song <thruthesky@gmail.com>
2022-03-30 12:49:07 +0900

    update

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -3,17 +3,29 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    mocha: true,
   },
-  extends: ["eslint:recommended", "google"],
-  rules: {
-    "max-len": "off",
-    "valid-jsdoc": "off",
-    "require-jsdoc": "off",
-    "quotes": ["error", "double"],
-  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    // ecmaVersion: 8, // or 2017
-    ecmaVersion: 2020,
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
   },
 };
