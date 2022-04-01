810536a9ffc83170bc3c741da6a261dbcad28951
JaeHo Song <thruthesky@gmail.com>
2022-04-01 20:16:17 +0900

    merge. lint

--- firebase/functions/tests/comment/comment-send-message-on-create.spec.ts
+++ firebase/functions/tests/comment/comment-send-message-on-create.spec.ts
@@ -1,12 +1,11 @@
 import "mocha";
 
 import { FirebaseAppInitializer } from "../firebase-app-initializer";
-import { Utils } from "../../src/classes/utils";
 
 new FirebaseAppInitializer();
 
 describe("Send message on comment create test", () => {
   it("Sende Message on comment create", async () => {
-    const commentId = "commentTest" + Utils.getTimestamp();
+    // const commentId = "commentTest" + Utils.getTimestamp();
   });
 });
