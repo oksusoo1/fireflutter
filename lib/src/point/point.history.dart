3e0fa484693e1e65d812cf9ee52060a9d885b898
JaeHo Song <thruthesky@gmail.com>
2022-04-04 22:26:52 +0900

    update point history

--- lib/src/point/point.history.dart
+++ lib/src/point/point.history.dart
@@ -35,13 +35,28 @@ class _PointHistoryState extends State<PointHistory> {
         itemBuilder: ((context, index) {
           PointHistoryModel history = histories[index];
 
-          /// TODO: 보기 좋게 처리. eventName 을 친숙한 용어로 변경, timestamp 를 적절한 시간으로 변경.
           /// TODO: 년도와 날짜 선택 가능하게 처리
-          /// TODO: 각 목록의 처음 포인트와 맨 마지막 포인트까지 계산을 해서 이상이 있으면 표시 할 것. (이렇게 하기 위해서는 변경 후, 나의 포인트를 같이 기록해야 함.)
+          final d = DateTime.fromMillisecondsSinceEpoch(history.timestamp * 1000);
           return ListTile(
-            title: Text(history.eventName),
-            subtitle: Text("Point. ${history.point} at ${history.timestamp}"),
+            title: Text(_text(history.eventName)),
+            subtitle: Text(
+                "Point. ${history.point} at ${d.year}-${d.month}-${d.day} ${d.hour}:${d.minute}:${d.second}"),
           );
         }));
+  }
+
+  _text(String t) {
+    switch (t) {
+      case 'register':
+        return 'Registration bonus';
+      case 'signIn':
+        return 'Sign-in bonus';
+      case 'postCreate':
+        return 'Post creation bonus';
+      case 'commentCreate':
+        return 'Comment creation bonus';
+      default:
+        return t;
+    }
   }
 }
