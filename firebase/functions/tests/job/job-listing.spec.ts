e4e2ac850604d9a54b039f8b03f16308858874f6
Selrahc <pinedaclp@gmail.com>
2022-04-13 22:16:24 +0800

    update on joblist

---
+++ firebase/functions/tests/job/job-listing.spec.ts
@@ -0,0 +1,16 @@
+import "mocha";
+import { expect } from "chai";
+
+import { FirebaseAppInitializer } from "../firebase-app-initializer";
+import { Post } from "../../src/classes/post";
+import // ERROR_EMPTY_UID,
+"../../src/defines";
+
+import { Test } from "../../src/classes/test";
+import { Utils } from "../../src/classes/utils";
+
+new FirebaseAppInitializer();
+
+describe("Job listing test", () => {
+  it("Job listing test.", async () => {});
+});
