dfb4dfac71df04e0ce0525a4c3c69d076b0d218c
JaeHo Song <thruthesky@gmail.com>
2022-04-15 22:01:55 +0900

    update list options

--- lib/src/extra/job/job.list.options.dart
+++ lib/src/extra/job/job.list.options.dart
@@ -15,213 +15,248 @@ class _JobListOptionsState extends State<JobListOptions> with FirestoreMixin {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle optionStyle = TextStyle(fontSize: 13);
     return Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Column(
             children: [
-              Row(children: [
-                Expanded(
-                  child: TextField(
-                    controller:
-                        TextEditingController(text: options.companyName),
-                    decoration: InputDecoration(
-                      labelText: "Company name",
-                    ),
-                    onChanged: (v) {
-                      bounce('companyName', 500, (_) async {
-                        print('search via company name');
-                        options.companyName = v;
-                        widget.change(options);
-                      });
-                    },
-                  ),
-                ),
-              ]),
               Row(
                 children: [
-                  DropdownButton<String>(
-                    value: options.jobCategory,
-                    items: [
-                      DropdownMenuItem(
-                        child: Text('Job category'),
-                        value: '',
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        // labelText: "Company name",
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(6),
+                        hintText: 'Company',
                       ),
-                      ...JobService.instance.categories.entries
-                          .map((e) => DropdownMenuItem(
-                                child: Text(e.value),
-                                value: e.key,
-                              ))
-                          .toList(),
-                    ],
-                    onChanged: (v) {
-                      setState(() {
-                        options.jobCategory = v ?? '';
-                      });
-                      widget.change(options);
-                    },
+                      onChanged: (v) {
+                        bounce('companyName', 500, (_) async {
+                          print('search via company name');
+                          options.companyName = v;
+                          widget.change(options);
+                        });
+                      },
+                    ),
                   ),
-                  DropdownButton<String>(
-                      value: options.salary,
+                  SizedBox(width: 8),
+                  Expanded(
+                    flex: 3,
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: options.jobCategory,
                       items: [
                         DropdownMenuItem(
-                          child: Text('Select salary'),
+                          child: Text('Industry'),
                           value: '',
                         ),
-                        ...JobService.instance.salaries.map(
-                          (s) => DropdownMenuItem(
-                            child: Text("$s Won"),
-                            value: s,
-                          ),
-                        )
+                        ...JobService.instance.categories.entries
+                            .map((e) => DropdownMenuItem(
+                                  child: Text(e.value, style: optionStyle),
+                                  value: e.key,
+                                ))
+                            .toList(),
                       ],
-                      onChanged: (s) {
+                      onChanged: (v) {
                         setState(() {
-                          options.salary = s ?? "";
+                          options.jobCategory = v ?? '';
                         });
-
                         widget.change(options);
-                      }),
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    flex: 2,
+                    child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: options.salary,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text('Salary'),
+                            value: '',
+                          ),
+                          ...JobService.instance.salaries.map(
+                            (s) => DropdownMenuItem(
+                              child: Text("$s Won", style: optionStyle),
+                              value: s,
+                            ),
+                          )
+                        ],
+                        onChanged: (s) {
+                          setState(() {
+                            options.salary = s ?? "";
+                          });
+
+                          widget.change(options);
+                        }),
+                  ),
                 ],
               ),
               Row(
                 children: [
-                  DropdownButton<String>(
-                    value: options.siNm,
-                    items: [
-                      DropdownMenuItem(
-                        child: Text('Select location'),
-                        value: '',
-                      ),
-                      for (final name in JobService.instance.areas.keys)
-                        DropdownMenuItem(
-                          child: Text(name),
-                          value: name,
-                        )
-                    ],
-                    onChanged: (s) {
-                      setState(
-                        () {
-                          if (options.siNm != s) {
-                            options.sggNm = '';
-                          }
-                          options.siNm = s ?? '';
-                        },
-                      );
-
-                      widget.change(options);
-                    },
-                  ),
-                  if (options.siNm != '')
-                    DropdownButton<String>(
-                      value: options.sggNm,
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: options.siNm,
                       items: [
                         DropdownMenuItem(
-                          child: Text('Select city/county/gu'),
+                          child: Text('Select location'),
                           value: '',
                         ),
-                        for (final name
-                            in JobService.instance.areas[options.siNm]!)
+                        for (final name in JobService.instance.areas.keys)
                           DropdownMenuItem(
                             child: Text(name),
                             value: name,
                           )
                       ],
                       onChanged: (s) {
-                        setState(() {
-                          options.sggNm = s ?? '';
-                        });
+                        setState(
+                          () {
+                            if (options.siNm != s) {
+                              options.sggNm = '';
+                            }
+                            options.siNm = s ?? '';
+                          },
+                        );
 
                         widget.change(options);
                       },
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  if (options.siNm != '')
+                    Expanded(
+                      flex: 1,
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: options.sggNm,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text('Select city/county/gu'),
+                            value: '',
+                          ),
+                          for (final name
+                              in JobService.instance.areas[options.siNm]!
+                                ..sort((a, b) => a.compareTo(b)))
+                            DropdownMenuItem(
+                              child: Text(name),
+                              value: name,
+                            )
+                        ],
+                        onChanged: (s) {
+                          setState(() {
+                            options.sggNm = s ?? '';
+                          });
+
+                          widget.change(options);
+                        },
+                      ),
                     ),
                 ],
               ),
               Row(
                 children: [
-                  DropdownButton<int>(
-                      value: options.workingHours,
-                      items: [
-                        DropdownMenuItem(
-                          child: Text('Working hours'),
-                          value: -1,
-                        ),
-                        DropdownMenuItem(
-                          child: Text('Flexible'),
-                          value: 0,
-                        ),
-                        DropdownMenuItem(
-                          child: Text('1 hour'),
-                          value: 1,
-                        ),
-                        for (int i = 2; i <= 14; i++)
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton<int>(
+                        isExpanded: true,
+                        value: options.workingHours,
+                        items: [
                           DropdownMenuItem(
-                            child: Text('$i hours'),
-                            value: i,
+                            child: Text('Working hours', style: optionStyle),
+                            value: -1,
                           ),
-                      ],
-                      onChanged: (n) {
-                        // print('s; $s');
-                        setState(() {
-                          options.workingHours = n ?? 0;
-                        });
-
-                        widget.change(options);
-                      }),
-                  DropdownButton<int>(
-                      value: options.workingDays,
-                      items: [
-                        DropdownMenuItem(
-                          child: Text('Working days'),
-                          value: -1,
-                        ),
-                        DropdownMenuItem(
-                          child: Text('Flexible'),
-                          value: 0,
-                        ),
-                        DropdownMenuItem(
-                          child: Text('1 day'),
-                          value: 1,
-                        ),
-                        for (int i = 2; i <= 7; i++)
                           DropdownMenuItem(
-                            child: Text('$i days'),
-                            value: i,
+                            child: Text('Flexible'),
+                            value: 0,
                           ),
-                      ],
-                      onChanged: (n) {
-                        // print('s; $s');
-                        setState(() {
-                          options.workingDays = n ?? 0;
-                        });
+                          DropdownMenuItem(
+                            child: Text('1 hour'),
+                            value: 1,
+                          ),
+                          for (int i = 2; i <= 14; i++)
+                            DropdownMenuItem(
+                              child: Text('$i hours'),
+                              value: i,
+                            ),
+                        ],
+                        onChanged: (n) {
+                          // print('s; $s');
+                          setState(() {
+                            options.workingHours = n ?? 0;
+                          });
 
-                        widget.change(options);
-                      }),
-                  DropdownButton<String>(
-                      value: options.accomodation,
-                      items: [
-                        DropdownMenuItem(
-                          child: Text('Accomodation?'),
-                          value: '',
-                        ),
-                        DropdownMenuItem(
-                          child: Text('Yes'),
-                          value: 'Y',
-                        ),
-                        DropdownMenuItem(
-                          child: Text('No'),
-                          value: "N",
-                        ),
-                      ],
-                      onChanged: (n) {
-                        // print('s; $s');
-                        setState(() {
-                          options.accomodation = n ?? '';
-                        });
+                          widget.change(options);
+                        }),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton<int>(
+                        isExpanded: true,
+                        value: options.workingDays,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text('Working days', style: optionStyle),
+                            value: -1,
+                          ),
+                          DropdownMenuItem(
+                            child: Text('Flexible'),
+                            value: 0,
+                          ),
+                          DropdownMenuItem(
+                            child: Text('1 day'),
+                            value: 1,
+                          ),
+                          for (int i = 2; i <= 7; i++)
+                            DropdownMenuItem(
+                              child: Text('$i days'),
+                              value: i,
+                            ),
+                        ],
+                        onChanged: (n) {
+                          // print('s; $s');
+                          setState(() {
+                            options.workingDays = n ?? 0;
+                          });
 
-                        widget.change(options);
-                      }),
+                          widget.change(options);
+                        }),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton<String>(
+                        isExpanded: true,
+                        value: options.accomodation,
+                        items: [
+                          DropdownMenuItem(
+                            child: Text('Accomodation?', style: optionStyle),
+                            value: '',
+                          ),
+                          DropdownMenuItem(
+                            child: Text('Yes'),
+                            value: 'Y',
+                          ),
+                          DropdownMenuItem(
+                            child: Text('No'),
+                            value: "N",
+                          ),
+                        ],
+                        onChanged: (n) {
+                          // print('s; $s');
+                          setState(() {
+                            options.accomodation = n ?? '';
+                          });
+
+                          widget.change(options);
+                        }),
+                  ),
                 ],
               ),
             ],
