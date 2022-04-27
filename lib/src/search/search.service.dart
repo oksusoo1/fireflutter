f8d8714b8d20d769c4f39cfefdf7b7be5bd8fc37
YinG17 <simbulanja.17@gmail.com>
2022-04-27 14:33:03 +0800

    search update

--- lib/src/search/search.service.dart
+++ lib/src/search/search.service.dart
@@ -1,5 +1,16 @@
 import 'package:meilisearch/meilisearch.dart';
 
+class SearchOptionModel {
+  String searchKey = '';
+  String index = 'posts';
+  String category = '';
+  String uid = '';
+  int limit = 20;
+  int offset = 0;
+  int page = 1;
+  List<String> sort = ['createdAt:desc'];
+}
+
 class SearchService {
   static SearchService? _instance;
   static SearchService get instance {
@@ -14,9 +25,8 @@ class SearchService {
 
   MeiliSearchClient get client => MeiliSearchClient(_serverUrl, _apiKey);
 
-  List<Map<String, dynamic>> resultList = [];
-
-  bool noMorePosts = false;
+  // List<Map<String, dynamic>> resultList = [];
+  // bool noMorePosts = false;
 
   /// Search options
   ///
@@ -24,17 +34,17 @@ class SearchService {
   /// [offset] is the number of documents to skip.
   /// If [sort] is not null, it will sort search results by an attribute's value.
   ///
-  String searchKey = '';
-  String index = '';
-  String category = '';
-  String uid = '';
-  int limit = 20;
-  int offset = 0;
-  int page = 1;
-  List<String> sort = ['createdAt:desc'];
+  // String searchKey = '';
+  // String index = '';
+  // String category = '';
+  // String uid = '';
+  // int limit = 20;
+  // int offset = 0;
+  // int page = 1;
+  // List<String> sort = ['createdAt:desc'];
 
-  int _hits = 0;
-  int get hits => _hits;
+  // int _hits = 0;
+  // int get hits => _hits;
 
   init({required String serverUrl, String? apiKey}) {
     _serverUrl = serverUrl;
@@ -43,65 +53,66 @@ class SearchService {
 
   /// Searches for indexed documents in the given `_serverUrl`.
   ///
-  Future<List<Map<String, dynamic>>> search() async {
-    if (noMorePosts) return [];
+  Future<SearchResult> search(SearchOptionModel opts) async {
+    // if (noMorePosts) return [];
     // print('Fetching posts');
     // print('limit ---> $limit');
     // print('offset ---> $offset');
     // print('page ---> $page');
 
     List filters = [];
-    if (uid.isNotEmpty) filters.add('uid = $uid');
-    if (category.isNotEmpty && index != 'comments')
-      filters.add('category = $category');
+    if (opts.uid.isNotEmpty) filters.add('uid = ${opts.uid}');
+    if (opts.category.isNotEmpty && opts.index != 'comments')
+      filters.add('category = ${opts.category}');
 
-    List<Map<String, dynamic>> _posts = [];
-    final SearchResult res = await client.index(index).search(
-          searchKey,
-          limit: limit,
-          offset: offset,
-          sort: sort,
+    return await client.index(opts.index).search(
+          opts.searchKey,
+          limit: opts.limit,
+          offset: opts.offset,
+          sort: opts.sort,
           filter: filters,
         );
 
-    if (res.hits != null) {
-      if (res.hits!.length < limit) noMorePosts = true;
+    // List<Map<String, dynamic>> _posts = [];
+    // final SearchResult res = await client.index(opts.index).search(
+    //       opts.searchKey,
+    //       limit: opts.limit,
+    //       offset: opts.offset,
+    //       sort: sort,
+    //       filter: filters,
+    //     );
 
-      _posts = res.hits!;
-      _hits = res.nbHits ?? 0;
+    // if (res.hits != null) {
+    // if (res.hits!.length < limit) noMorePosts = true;
 
-      ///
-      resultList.addAll(_posts);
-      offset = limit * page;
-      page += 1;
-    }
+    // _posts = res.hits!;
+    // _hits = res.nbHits ?? 0;
 
-    return _posts;
+    ///
+    // resultList.addAll(_posts);
+    // offset = limit * page;
+    // page += 1;
+    // }
+
+    // return _posts;
   }
 
-  /// Deletes all documents of an index.
-  ///
-  Future deleteAllDocuments(String uid) async {
-    /// if (!UserService.instance.user.isAdmin) throw 'YOU_ARE_NOT_ADMIN';
-    return client.index(uid).deleteAllDocuments();
-  }
+  // resetFilters({String index = ''}) {
+  //   uid = '';
+  //   category = '';
+  //   index = index;
+  //   searchKey = '';
+  //   sort = ['createdAt:desc'];
+  // }
 
-  resetFilters({String index = ''}) {
-    uid = '';
-    category = '';
-    index = index;
-    searchKey = '';
-    sort = ['createdAt:desc'];
-  }
+  // resetListAndPagination({int limit = 20}) {
+  //   // noMorePosts = false;
+  //   limit = limit;
+  //   offset = 0;
+  //   page = 1;
 
-  resetListAndPagination({int limit = 20}) {
-    noMorePosts = false;
-    limit = limit;
-    offset = 0;
-    page = 1;
-
-    resultList = [];
-  }
+  //   // resultList = [];
+  // }
 
   /// Returns a total count of documents a user owned in the given index.
   ///
