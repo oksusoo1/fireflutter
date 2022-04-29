d63dd3bb139854375e7e80526ba5ffe28374827a
JaeHo Song <thruthesky@gmail.com>
2022-04-29 22:54:08 +0900

    update on admin screen & reprot

--- fe/lib/screens/admin/admin.screen.dart
+++ fe/lib/screens/admin/admin.screen.dart
@@ -32,70 +32,73 @@ class _AdminScreenState extends State<AdminScreen> {
       appBar: AppBar(
         title: const Text('Admin Screen'),
       ),
-      body: PagePadding(
-        children: [
-          UserService.instance.user.isAdmin
-              ? const Text('You are an admin')
-              : const Text('You are not admin'),
-          Divider(),
-          Text('Forum Management'),
-          Wrap(
-            children: [
-              ElevatedButton(
-                onPressed: AppService.instance.openCategoryGroup,
-                child: Text('Category Group'),
-              ),
-              spaceXxs,
-              ElevatedButton(
-                onPressed: AppService.instance.openCategory,
-                child: const Text('Category'),
-              ),
-            ],
-          ),
-          Divider(),
-          ElevatedButton(
-              onPressed: AppService.instance.openTranslations, child: Text('Update Translations')),
-          Divider(),
-          Text('Report Management'),
-          Wrap(
-            children: [
-              ElevatedButton(
-                onPressed: AppService.instance.openReport,
-                child: const Text('All'),
-              ),
-              ElevatedButton(
-                onPressed: () => AppService.instance.openReport('post'),
-                child: const Text('Posts'),
-              ),
-              ElevatedButton(
-                onPressed: () => AppService.instance.openReport('comment'),
-                child: const Text('Comments'),
-              ),
-              ElevatedButton(
-                onPressed: () => AppService.instance.openReport('user'),
-                child: const Text('Users'),
-              ),
-            ],
-          ),
-          Divider(),
-          Wrap(
-            children: [
-              ElevatedButton(
-                onPressed: () => AppService.instance.open('/pushNotification'),
-                child: const Text('Push Notification'),
-              ),
-              ElevatedButton(
-                onPressed: () => AppService.instance
-                    .open('/pushNotification', arguments: {'postId': '0EWGGe64ckjBtiU1LeB1'}),
-                child: const Text('Push Notification with postId'),
-              )
-            ],
-          ),
-          ElevatedButton(
-              onPressed: () => alert(
-                  'Create test users', "Run \$ node create.test.user.js in firebase/lab foler."),
-              child: Text('Create test users')),
-        ],
+      body: AdminOnly(
+        child: PagePadding(
+          children: [
+            UserService.instance.user.isAdmin
+                ? const Text('You are an admin')
+                : const Text('You are not admin'),
+            Divider(),
+            Text('Forum Management'),
+            Wrap(
+              children: [
+                ElevatedButton(
+                  onPressed: AppService.instance.openCategoryGroup,
+                  child: Text('Category Group'),
+                ),
+                spaceXxs,
+                ElevatedButton(
+                  onPressed: AppService.instance.openCategory,
+                  child: const Text('Category'),
+                ),
+              ],
+            ),
+            Divider(),
+            ElevatedButton(
+                onPressed: AppService.instance.openTranslations,
+                child: Text('Update Translations')),
+            Divider(),
+            Text('Report Management'),
+            Wrap(
+              children: [
+                ElevatedButton(
+                  onPressed: AppService.instance.openReport,
+                  child: const Text('All'),
+                ),
+                ElevatedButton(
+                  onPressed: () => AppService.instance.openReport('post'),
+                  child: const Text('Posts'),
+                ),
+                ElevatedButton(
+                  onPressed: () => AppService.instance.openReport('comment'),
+                  child: const Text('Comments'),
+                ),
+                ElevatedButton(
+                  onPressed: () => AppService.instance.openReport('user'),
+                  child: const Text('Users'),
+                ),
+              ],
+            ),
+            Divider(),
+            Wrap(
+              children: [
+                ElevatedButton(
+                  onPressed: () => AppService.instance.open('/pushNotification'),
+                  child: const Text('Push Notification'),
+                ),
+                ElevatedButton(
+                  onPressed: () => AppService.instance
+                      .open('/pushNotification', arguments: {'postId': '0EWGGe64ckjBtiU1LeB1'}),
+                  child: const Text('Push Notification with postId'),
+                )
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () => alert(
+                    'Create test users', "Run \$ node create.test.user.js in firebase/lab foler."),
+                child: Text('Create test users')),
+          ],
+        ),
       ),
     );
   }
--- fe/lib/screens/admin/report.screen.dart
+++ fe/lib/screens/admin/report.screen.dart
@@ -18,24 +18,26 @@ class ReportScreen extends StatelessWidget with FirestoreMixin {
       appBar: AppBar(
         title: const Text('Report Management'),
       ),
-      body: ReportManagement(
-        target: arguments['target'],
-        // onError: error,
-        onPressed: (ReportModel report) async {
-          if (report.target == 'post') {
-            AppService.instance.openPostView(id: report.targetId);
-          } else if (report.target == 'comment') {
-            final doc = await commentCol.doc(report.targetId).get();
-            final CommentModel comment = CommentModel.fromJson(doc.data() as Json, id: doc.id);
-            AppService.instance.openPostView(id: comment.postId);
-          } else if (report.target == 'user') {
-            // user profiler management
-          } else if (report.target == 'image') {
-            // file managet
-          } else {
-            AppService.instance.openReportForumMangement(report.target, report.targetId);
-          }
-        },
+      body: AdminOnly(
+        child: ReportManagement(
+          target: arguments['target'],
+          // onError: error,
+          onPressed: (ReportModel report) async {
+            if (report.target == 'post') {
+              AppService.instance.openPostView(id: report.targetId);
+            } else if (report.target == 'comment') {
+              final doc = await commentCol.doc(report.targetId).get();
+              final CommentModel comment = CommentModel.fromJson(doc.data() as Json, id: doc.id);
+              AppService.instance.openPostView(id: comment.postId);
+            } else if (report.target == 'user') {
+              // user profiler management
+            } else if (report.target == 'image') {
+              // file managet
+            } else {
+              AppService.instance.openReportForumMangement(report.target, report.targetId);
+            }
+          },
+        ),
       ),
     );
   }
