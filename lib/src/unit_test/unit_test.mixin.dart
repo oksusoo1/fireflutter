1b8de7ce0231d676d1b6297b54969e3cd58c5f76
JaeHo Song <thruthesky@gmail.com>
2022-04-29 23:23:09 +0900

    update on test

--- lib/src/unit_test/unit_test.mixin.dart
+++ lib/src/unit_test/unit_test.mixin.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:fireflutter/fireflutter.dart';
+import '../../fireflutter.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/subjects.dart';
 
