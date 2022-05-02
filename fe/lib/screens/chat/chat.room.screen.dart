601149fa220c21f4cc360689d6c6f40833fb82b1
JaeHo Song <thruthesky@gmail.com>
2022-05-02 19:22:58 +0900

    update for fe chat

--- fe/lib/screens/chat/chat.room.screen.dart
+++ fe/lib/screens/chat/chat.room.screen.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:extended/extended.dart';
 import 'package:fe/screens/chat/widgets/chat_room.message.dart';
 import 'package:fe/screens/chat/widgets/chat_room.message_box.dart';
 import 'package:fe/screens/friend_map/friend_map.screen.dart';
 import 'package:fe/services/app.service.dart';
+import 'package:fe/services/defines.dart';
+import 'package:fe/services/global.dart';
 import 'package:fireflutter/fireflutter.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -18,16 +23,105 @@ class ChatRoomScreen extends StatefulWidget {
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
+  late String otherUid;
+
+  bool blocked = false;
+
+  late StreamSubscription blockedRoomSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    otherUid = widget.arguments['uid'];
+
+    blockedRoomSubscription = ChatService.instance.myRoomsBlockedCol
+        .doc(otherUid)
+        .snapshots()
+        .listen((DocumentSnapshot snapshot) {
+      if (mounted)
+        setState(() {
+          if (snapshot.exists) {
+            blocked = true;
+          } else {
+            blocked = false;
+          }
+        });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    blockedRoomSubscription.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        shape: Border(bottom: BorderSide(color: Colors.grey.shade300)),
         title: UserDoc(
           uid: widget.arguments['uid'],
           builder: (UserModel user) {
-            return Text(user.displayName);
+            return Text(user.displayName, style: titleStyle);
           },
         ),
+        actions: [
+          ChatRoomPushNotificationIcon(
+            otherUid,
+            size: 28,
+          ),
+          UserDoc(
+              uid: otherUid,
+              builder: (user) {
+                return Popup(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                  ),
+                  initialValue: '',
+                  options: {
+                    'friendMap': PopupOption(icon: const Icon(Icons.map), label: 'Friend Map'),
+                    if (blocked) 'unblock': PopupOption(icon: Icon(Icons.block), label: 'Unblock'),
+                    if (!blocked) 'block': PopupOption(icon: Icon(Icons.block), label: 'Block'),
+                    'report': PopupOption(icon: Icon(Icons.report), label: 'Report'),
+                    'close': PopupOption(icon: Icon(Icons.close), label: 'Close'),
+                  },
+                  onSelected: (v) async {
+                    if (v == 'friendMap') {
+                      // service.requestFriendMap(user);
+                      alert('@todo - friend map', 'friend map todo');
+                    } else if (v == 'block') {
+                      final re = await service.confirm('Block', 'Do you want to block this user?');
+                      if (re == false) return;
+                      // block user
+                      ChatService.instance.blockUser(otherUid);
+                      service.router.back();
+                    } else if (v == 'unblock') {
+                      final re =
+                          await service.confirm('Block', 'Do you want to unblock this user?');
+                      if (re == false) return;
+                      // block user
+                      ChatService.instance.unblockUser(otherUid);
+                      service.router.back();
+                    } else if (v == 'report') {
+                      String? re = await inputDialog('Report User', 'Reason');
+                      if (re == null) return;
+                      // save report to backend
+                      await ReportApi.instance.report(
+                        target: 'chat',
+                        targetId: getChatRoomDocumentId(UserService.instance.uid, otherUid),
+                        reason: re,
+                      );
+                      service.alert('Reported', 'You have been reported this user successfully.');
+                    }
+                  },
+                );
+              }),
+        ],
       ),
       body: Auth(
           signedOut: () => const Text('login first'),
