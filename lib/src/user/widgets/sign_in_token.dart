f4d148b7e9b2da74afa40d9824c8ae27d424fb17
JaeHo Song <thruthesky@gmail.com>
2022-05-10 22:37:16 +0900

    update on tokens

--- lib/src/user/widgets/sign_in_token.dart
+++ lib/src/user/widgets/sign_in_token.dart
@@ -1,4 +1,4 @@
-import 'package:fireflutter/fireflutter.dart';
+import '../../../fireflutter.dart';
 import 'package:flutter/material.dart';
 
 class SignInToken extends StatefulWidget {
@@ -45,6 +45,7 @@ class _SignInTokenState extends State<SignInToken> with DatabaseMixin {
         Text(
           'Input this code to login on the website.',
         ),
+        SizedBox(height: 16),
         Text(
           id,
           style: TextStyle(
